af34c961418fff65744e381e104f40c93e2c2c48
Hasnat <webdevhasnat@gmail.com>
2022-10-21 14:19:52 +0600

    fdfsfsf

--- src/components/BodyContent.js
+++ src/components/BodyContent.js
@@ -15,7 +15,7 @@ const BodyContent = (props) => {
           <p>{data.length} Reviews </p>
           <p>Average rating : {isFinite(average) ? average : 0} </p>
         </div>
-        <motion className="review-card">
+        <div className="review-card">
           {data.map((review) => (
             <ReviewCard
               onRemoveReview={props.onRemove}
@@ -23,7 +23,7 @@ const BodyContent = (props) => {
               key={review.id}
             />
           ))}
-        </motion>
+        </div>
       </div>
     </section>
   );
